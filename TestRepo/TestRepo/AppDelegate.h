ab211ade2fbf1db4b9171b1562f88250f4c5cf53
jinshan.zhang <jinshan.zhang@51ysscw.com>
2019-12-12 17:06:34 +0800

    修改

---
+++ TestRepo/TestRepo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestRepo
+//
+//  Created by 张金山 on 2019/12/12.
+//  Copyright © 2019 张金山. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestRepo/TestRepo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestRepo
+//
+//  Created by 张金山 on 2019/12/12.
+//  Copyright © 2019 张金山. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
